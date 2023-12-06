0eb288ba8945e9f2a5395550fedfdba2a5af08eb
Golftion <Patiphon.Songkhorthum@minebea.com>
2023-12-06 16:09:18 +0700

    06/12/2566 Training the new member

--- App/GrandSubTotal/GrandSubTotal.aspx
+++ App/GrandSubTotal/GrandSubTotal.aspx
@@ -11,13 +11,12 @@
     <form id="form1" runat="server">
         <h1>Grand and Sub Total</h1>
         <div>
-            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnDataBound="OnDataBound">
-                <AlternatingRowStyle BackColor="White" />
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnDataBound="OnDataBound" >
+                <AlternatingRowStyle BackColor="#FFFFFF" />
                 <EditRowStyle BackColor="#2461BF" />
-                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
-                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <FooterStyle BackColor="#F875AA" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#F875AA" Font-Bold="True" ForeColor="White" />
                 <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
-                <RowStyle BackColor="#EFF3FB" />
                 <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
                 <SortedAscendingCellStyle BackColor="#F5F7FB" />
                 <SortedAscendingHeaderStyle BackColor="#6D95E1" />
--- App/GrandSubTotal/GrandSubTotal.aspx.vb
+++ App/GrandSubTotal/GrandSubTotal.aspx.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Drawing
+﻿Imports System.ComponentModel
+Imports System.Drawing
 Imports System.IO
 Imports System.Web.Script.Serialization
 Imports Newtonsoft.Json
@@ -6,8 +7,10 @@ Imports Newtonsoft.Json
 Public Class GrandSubTotal
     Inherits System.Web.UI.Page
 
+    Private MainDataTable As New DataTable
+
     Private GrandTotal As Decimal = 0
-    Private SubTotal As Decimal = 0
+    Private SubToTal As Decimal = 0
     Private Model_SubTotal As Decimal = 0
 
     Private CurrentBrand As String = ""
@@ -15,95 +18,101 @@ Public Class GrandSubTotal
 
     Private CurrentIndex As Integer = 0
 
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        GridView1.Visible = False
-
-        Dim DataTable As DataTable = ReadDataFromJsonFile()
-        If DataTable.Rows.Count <> 0 Then
-            GridView1.DataSource = DataTable
-            GridView1.DataBind()
-            GridView1.Visible = True
+        If Not IsPostBack Then
+            BindGrid()
         End If
 
     End Sub
 
     Private Function ReadDataFromJsonFile() As DataTable
-        Dim JsonPath As String = "P:\VBNet Project\VBNet Training\App\GrandSubTotal\Data.json"
-        ' Read the JSON file
-        Dim jsonContent As String
+
+        Dim JsonPath As String = "D:\Web Servers\Training\App\GrandSubTotal\Data.json"
+        Dim JsonContent As String
         Using Reader As New StreamReader(JsonPath)
-            jsonContent = Reader.ReadToEnd()
+            JsonContent = Reader.ReadToEnd
         End Using
 
-        ' Deserialize the JSON content using Json.NET
-        'Dim carSales = JsonConvert.DeserializeObject(Of List(Of CarSales))(jsonContent)
-
-        'Convert a JSON data to a DataTable 
-        Dim DataTable As DataTable = JsonConvert.DeserializeObject(Of DataTable)(jsonContent)
-        DataTable.DefaultView.Sort = "Brand ASC, Model ASC, Quarter ASC"
+        Dim DataTable As DataTable = JsonConvert.DeserializeObject(Of DataTable)(JsonContent)
+        DataTable.DefaultView.Sort = "Brand ASC, Model ASC , Quarter ASC"
         Return DataTable
     End Function
 
-    Private Sub AddSubTotalRow(ByVal Text As String, ByVal Value As String, ByVal Type As Integer)
+    Private Sub BindGrid()
+        GridView1.Visible = False
+        MainDataTable = ReadDataFromJsonFile()
+
+        If MainDataTable.Rows.Count > 0 Then
+            GridView1.DataSource = MainDataTable
+            GridView1.DataBind()
+            GridView1.Visible = True
+        End If
+    End Sub
+
+    Private Sub AddRow(ByVal Text As String, ByVal Value As String, ByVal Type As Integer)
         Dim row As New GridViewRow(0, 0, DataControlRowType.DataRow, DataControlRowState.Normal)
 
         Select Case Type
             Case 1
-                row.BackColor = ColorTranslator.FromHtml("#F9F9F9")
-                row.Cells.AddRange(New TableCell(3) {New TableCell(),
+                row.BackColor = ColorTranslator.FromHtml("#FFDFDF")
+                row.Cells.AddRange(New TableCell(3) {
                 New TableCell() With {
-                  .Text = "",
-                  .HorizontalAlign = HorizontalAlign.Left
+                    .Text = Text,
+                    .HorizontalAlign = HorizontalAlign.Left
                 },
-                 New TableCell() With {
-                  .Text = Text,
-                  .HorizontalAlign = HorizontalAlign.Left
-                }, New TableCell() With {
-                  .Text = Value,
-                  .HorizontalAlign = HorizontalAlign.Right
+                New TableCell() With {
+                    .Text = "",
+                    .HorizontalAlign = HorizontalAlign.Left
+                },
+                New TableCell() With {
+                    .Text = "",
+                    .HorizontalAlign = HorizontalAlign.Left
+                },
+                New TableCell() With {
+                    .Text = Value,
+                    .HorizontalAlign = HorizontalAlign.Right
                 }})
+
             Case 2
-                row.BackColor = ColorTranslator.FromHtml("#F9F9F9")
+                row.BackColor = ColorTranslator.FromHtml("#FFF6F6")
                 row.Cells.AddRange(New TableCell(3) {New TableCell(),
                 New TableCell() With {
-                  .Text = Text,
-                  .HorizontalAlign = HorizontalAlign.Left
+                    .Text = Text,
+                    .HorizontalAlign = HorizontalAlign.Left
                 },
-                 New TableCell() With {
-                  .Text = "",
-                  .HorizontalAlign = HorizontalAlign.Right
-                }, New TableCell() With {
-                  .Text = Value,
-                  .HorizontalAlign = HorizontalAlign.Right
+                New TableCell() With {
+                    .Text = "",
+                    .HorizontalAlign = HorizontalAlign.Left
+                },
+                New TableCell() With {
+                    .Text = Value,
+                    .HorizontalAlign = HorizontalAlign.Right
                 }})
+
             Case Else
         End Select
         GridView1.Controls(0).Controls.Add(row)
     End Sub
-
     Private Sub GridView1_RowCreated(sender As Object, e As GridViewRowEventArgs) Handles GridView1.RowCreated
-
         If e.Row.RowType = DataControlRowType.DataRow Then
-
-            Dim Sales As String = Convert.ToInt32(DataBinder.Eval(e.Row.DataItem, "Sales"))
-            Dim PreviousBrand As String = DataBinder.Eval(e.Row.DataItem, "Brand").ToString()
+            Dim Sales As Decimal = Convert.ToDecimal(DataBinder.Eval(e.Row.DataItem, "Sales"))
+            Dim PreviousBrand As String = DataBinder.Eval(e.Row.DataItem, "Brand").ToString
             Dim PreviousModel As String = DataBinder.Eval(e.Row.DataItem, "Model").ToString()
 
             If e.Row.RowIndex > 0 Then
 
                 If Not CurrentModel.Equals(PreviousModel) Or Not CurrentBrand.Equals(PreviousBrand) Then
-                    AddSubTotalRow("Subtotal of " & CurrentBrand & " : " & CurrentModel & "", Model_SubTotal.ToString("N2"), 2)
+                    AddRow("Subtotal of " & CurrentBrand & " : " & CurrentModel & "", Model_SubTotal.ToString("N2"), 2)
                     Model_SubTotal = 0
                     CurrentModel = PreviousModel
                     CurrentIndex = e.Row.RowIndex 'To get the last model row for handling, show the last row subtotal.
                 End If
 
                 If Not CurrentBrand.Equals(PreviousBrand) Then
-                    AddSubTotalRow("Subtotal of " & CurrentBrand & "", SubTotal.ToString("N2"), 1)
-                    GrandTotal += SubTotal
-                    SubTotal = 0
+                    AddRow("Subtotal of " & CurrentBrand & "", SubToTal.ToString("N2"), 1)
+                    GrandTotal += SubToTal
+                    SubToTal = 0
                     Model_SubTotal = 0
                     CurrentBrand = PreviousBrand
                     CurrentModel = PreviousModel
@@ -114,28 +123,31 @@ Public Class GrandSubTotal
                 CurrentModel = PreviousModel
             End If
 
-            SubTotal = SubTotal + Sales
+            SubToTal = SubToTal + Sales
             Model_SubTotal += Sales
         End If
-
     End Sub
 
+    'Handling the last row not showing
     Protected Sub OnDataBound(sender As Object, e As EventArgs)
         Model_SubTotal = 0
         For I As Integer = CurrentIndex To GridView1.Rows.Count - 1
             Model_SubTotal += Convert.ToDecimal(GridView1.Rows(I).Cells(3).Text)
         Next
-        Me.AddSubTotalRow("Subtotal of " & CurrentBrand & " : " & CurrentModel & "", Model_SubTotal.ToString("N2"), 2)
-        Me.AddSubTotalRow("Subtotal of " & CurrentBrand & "", SubTotal.ToString("N2"), 1)
-        GrandTotal += SubTotal
-        Me.AddSubTotalRow("Grandtotal of sales", GrandTotal.ToString("N2"), 1)
+        AddRow("Subtotal of " & CurrentBrand & " : " & CurrentModel & "", Model_SubTotal.ToString("N2"), 2)
+        AddRow("Subtotal of " & CurrentBrand & "", SubToTal.ToString("N2"), 1)
+        GrandTotal += SubToTal
+        AddRow("Grandtotal of sales", GrandTotal.ToString("N2"), 1)
     End Sub
 
-    Public Class CarSales
-        Public Property Brand As String
-        Public Property Model As String
-        Public Property Quarter As Integer
-        Public Property Sales As Integer
-    End Class
+    Private Sub GridView1_RowDataBound(sender As Object, e As GridViewRowEventArgs) Handles GridView1.RowDataBound
 
+        If e.Row.RowType = DataControlRowType.DataRow Then
+
+            e.Row.Cells(2).HorizontalAlign = HorizontalAlign.Center
+
+            e.Row.Cells(3).HorizontalAlign = HorizontalAlign.Right
+            e.Row.Cells(3).Text = Decimal.Parse(e.Row.Cells(3).Text).ToString("N2")
+        End If
+    End Sub
 End Class
